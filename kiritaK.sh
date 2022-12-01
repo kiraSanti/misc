8f26821576542c5978825125ed82ca3a919ccfc3
Kira <115056771+kiraSanti@users.noreply.github.com>
2022-12-01 11:18:49 -0500

    Cow emoji added, capitalized T, no final line

--- kiritaK.sh
+++ kiritaK.sh
@@ -1,1 +1,1 @@
-echo "Kirita hermosa, mi vaquita... te extraño mucho, te mando un besito y un abrazo 🐕❤️" | lolcat
+echo "Kirita hermosa, mi vaquita 🐄 Te extraño mucho, te mando un besito y un abrazo 🐕❤️" | lolcat
